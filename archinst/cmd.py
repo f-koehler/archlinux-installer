e3a16cfef0a1e9b7db0dd881b0bde30530e82680
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-05-08 22:30:19 +0200

    Try to disable key checking in git clone function.

--- archinst/cmd.py
+++ archinst/cmd.py
@@ -1,11 +1,17 @@
+import os
 import subprocess
 from pathlib import Path
-from typing import List, Union
+from typing import Dict, List, Optional, Union
 
 
-def run(command: List[str]):
-    subprocess.run(command, check=True)
+def run(command: List[str], environment: Optional[Dict[str, str]] = None):
+    env = os.environ.copy()
+    if environment:
+        for key in environment:
+            env[key] = environment[key]
+    subprocess.run(command, check=True, env=env)
 
 
-def run_chroot(command: List[str], root: Union[str, Path] = "/mnt"):
-    run(["arch-chroot", str(root)] + command)
+def run_chroot(command: List[str],
+               environment: Optional[Dict[str, str]] = None):
+    run(["arch-chroot", "/mnt"] + command, environment)
