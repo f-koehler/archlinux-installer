a33567207429d3ecacaf418b47f4503f966fc2f6
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-05-08 23:04:28 +0200

    Git clone using HTTPS instead of SSH as key is not setup in the installer.

--- vm.py
+++ vm.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         grub.install_grub_efi(disk)
 
         user.add_normal_user("fkoehler")
-        git.clone("git@github.com:f-koehler/playbooks.git",
+        git.clone("https://github.com/f-koehler/playbooks.git",
                   "/home/fkoehler/code/playbooks")
-        git.clone("git@github.com:f-koehler/dotfiles.git",
+        git.clone("https://github.com/f-koehler/dotfiles.git",
                   "/home/fkoehler/code/dotfiles")
