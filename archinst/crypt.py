9b4afe21664614d26d83c9c65c9b5cba31fa7630
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-05-11 00:43:27 +0200

    Fix typo in `luksFormat` arguments.

--- archinst/crypt.py
+++ archinst/crypt.py
@@ -9,7 +9,7 @@ def create_luks_container(device: Union[str, Path], password: str):
         "cryptsetup", "-v", "luksFormat",
         str(device), "--type", "luks", "--cipher", "aes-xts-plain64",
         "--key-size", "512", "--hash", "sha256", "--iter-time", "5000",
-        "--use-urandom", "--keyfile", "-"
+        "--use-urandom", "--key-file", "-"
     ]
     process = subprocess.Popen(command,
                                stderr=sys.stderr,
