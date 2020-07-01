94092c77b6f0b4c517309dd29e7bb2f22a992358
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-07-01 19:06:50 +0200

    Go back to a non encrypted layout in the example; the encryption methods need to be refined first.

--- vm.py
+++ vm.py
@@ -12,45 +12,34 @@ if __name__ == "__main__":
 
     layout = part.PartitionLayout()
     layout.append("fat32", "200MiB", "/mnt/boot/efi")
-    layout.append("linux-swap", "2000MiB")
-    layout.append("btrfs", "100%")
+    layout.append("linux-swap", "2000MiB", "[SWAP]")
+    layout.append("btrfs", "100%", "/mnt")
     layout.apply(disk)
 
     fs.create_fs_vfat32(disk + "1", "efi")
+    fs.create_fs_swap(disk+"2", "arch_swap")
+    fs.create_fs_btrfs(disk+"3", "arch_root")
 
-    crypt.create_luks_container(disk + "3", "test")
-    with crypt.luks_open(disk + "3", "crypt_root", "test"):
-        fs.create_fs_btrfs("/dev/mapper/crypt_root", "arch_root")
-        # fs.create_fs_swap("/dev/mapper/crypt_swap", "arch_swap")
+    subvolumes = fs.BtrfsSubvolumes()
+    subvolumes.add("@", "/mnt/")
+    subvolumes.add("@home", "/mnt/home")
+    subvolumes.add("@snapshots", "/mnt/.snapshots")
+    subvolumes.add("@/var/log", "/mnt/var/log")
+    subvolumes.add("/var/cache/pacman/pkg")
+    subvolumes.apply("/dev/mapper/crypt_root")
 
-        subvolumes = fs.BtrfsSubvolumes()
-        subvolumes.add("@", "/mnt/")
-        subvolumes.add("@home", "/mnt/home")
-        subvolumes.add("@snapshots", "/mnt/.snapshots")
-        # subvolumes.add("@/var/log", "/mnt/var/log")
-        # subvolumes.add("/var/cache/pacman/pkg")
-        subvolumes.apply("/dev/mapper/crypt_root")
+    reflector.run_reflector(False, "Germany")
 
-        reflector.run_reflector(False, "Germany")
+    with subvolumes.mount("/dev/mapper/crypt_root"), layout.mount(disk):
+        pkg.pacstrap([
+            "base", "base-devel", "linux", "linux-firmware", "btrfs-progs",
+            "grub-btrfs"
+        ])
+        reflector.run_reflector(True, "Germany")
+        fs.generate_fs_table()
+        time.set_timezone("Europe/Berlin")
+        time.enable_ntp()
 
-        with subvolumes.mount("/dev/mapper/crypt_root"), layout.mount(disk):
-            pkg.pacstrap([
-                "base", "base-devel", "linux", "linux-firmware", "btrfs-progs",
-                "grub-btrfs"
-            ])
-            reflector.run_reflector(True, "Germany")
-            fs.generate_fs_table()
-            time.set_timezone("Europe/Berlin")
-            time.enable_ntp()
+        grub.install_grub_efi(disk)
 
-            hooks = initcpio.read_hooks()
-            initcpio.insert_hook_after(hooks, "udev", "base")
-            initcpio.insert_hook_after(hooks, "keyboard", "autodetect")
-            initcpio.insert_hook_after(hooks, "keymap", "keyboard")
-            initcpio.insert_hook_after(hooks, "encrypt", "block")
-            initcpio.set_hooks(hooks)
-            initcpio.mkinitcpio()
-
-            grub.install_grub_efi(disk)
-
-            user.add_normal_user("fkoehler")
+        user.add_normal_user("fkoehler")
