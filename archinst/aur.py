5e5a55393a03dc5fb424db9d50b3be917f586f9e
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2020-07-17 10:25:41 +0200

    Remove aur module.

--- archinst/aur.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from typing import Union
-from tempfile import TemporaryDirectory
-
-from archinst.cmd import run_chroot
-
-
-def install_yay(username: str, group: str, prefix: Union[str, Path] = "/mnt"):
