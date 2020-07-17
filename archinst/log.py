98185045c2a2182b0947ce2ab5716eba3e89a9de
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2020-07-17 17:13:49 +0200

    Open a log file.

--- archinst/log.py
+++ archinst/log.py
@@ -1,8 +1,15 @@
 # from logging import getLogger
 import logging
 from typing import Optional
+from pathlib import Path
+import sys
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=str(Path(sys.modules["__main__"].__file__).name) + ".log",
+    filemode="w",
+)
 
 
 def get_logger(name: Optional[str] = None):
